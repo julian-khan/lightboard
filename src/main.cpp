6e584ccd3b39e6f5ac303486e43b4305b9a523c4
Julian Khan <juliankhan.b@gmail.com>
2025-12-07 19:17:35 +0800

    save prog

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,98 @@
-// #include <Arduino.h>
-
-// // put function declarations here:
-// int myFunction(int, int);
-
-// void setup() {
-//   // put your setup code here, to run once:
-//   int result = myFunction(2, 3);
-// }
-
-// void loop() {
-//   // put your main code here, to run repeatedly:
-// }
-
-// // put function definitions here:
-// int myFunction(int x, int y) {
-//   return x + y;
-// }
-
 #include <Arduino.h>
 
+#define SWITCH_PIN 13
+
+#define LED_PIN_1 1      // Output pin for light bulb
+#define LED_PIN_2 2 
+#define LED_PIN_3 3
+#define LED_PIN_4 4
+#define LED_PIN_5 5
+#define LED_PIN_6 6
+
+
+// int leds[3] = {LED_PIN_1, LED_PIN_2, LED_PIN_3};
+
+// int gameLedSequence[] = {LED_PIN_1, LED_PIN_3, LED_PIN_5};
+// int currentGameIndex = 0;
+
+// // todo - fix the total game steps calculation
+// int totalGameSteps = sizeof(gameLedSequence);
+
 void setup() {
-  pinMode(1, OUTPUT);  // Digital pin 1 as output
+  Serial.begin(9600);
+  Serial.println("Lightboard Testm123");
+  // pinMode(SWITCH_PIN, INPUT_PULLUP);  // Switch with internal pull-up
+
+  //   for (int i = 0; i < (int)(sizeof(leds) / sizeof(leds[0])); i++) {
+  //        pinMode(leds[i], OUTPUT);
+  // }
+
+    // int targetLedPin = gameLedSequence[currentGameIndex];
+    //   Serial.println("Lightboard Testm123");
 }
 
+// void loop() {
+//   int switchState = digitalRead(SWITCH_PIN);  // Read switch (LOW when pressed)
+//   int targetLedPin = gameLedSequence[currentGameIndex];
+//       Serial.println("current pin - led: ");
+//       Serial.println(targetLedPin);
+
+//   // int ledState = digitalRead(targetLedPin);  // Read current bulb state
+
+//   digitalWrite(targetLedPin, HIGH);         // Bulb ON initially
+
+
+
+//   // Serial.print("Switch State: ");
+//   // Serial.print(switchState);
+//   // Serial.print(" targetledpin: ");
+//   // Serial.print(targetLedPin);
+//   // Serial.print(" ledstate: ");
+//   // // Serial.println(ledState);
+
+//   // // int ledState = digitalRead(LED_PIN_3);  // Read current bulb state
+//   // Serial.print(" test 1234: ");
+  
+
+
+//   if (switchState == HIGH) {                   // Button pressed
+//       Serial.print(" test 99999: ");
+//     digitalWrite(targetLedPin, LOW);              // Turn bulb ON
+//       delay(3000); // wait for 5 seconds
+
+//     // digitalWrite(LED_PIN_3, LOW);              // Turn bulb ON
+//     // delay(1000);                          // Debounce delay
+//     // digitalWrite(LED_PIN_3, HIGH);              // Turn bulb ON
+
+
+
+//       Serial.print(" current index: ");
+//             Serial.print(currentGameIndex);
+
+
+//     currentGameIndex++;
+//             Serial.print(currentGameIndex);
+
+
+//   } 
+
+// }
+
+
+// PIN 1 LIGHT HIGH
+
+
+// PIN 2 SWITCH - LOW -> PRESS -> HIGH 
+
+// PIN 1 -> HIGH FORM PIN 2 -> PIN 1 LOW
+
+
+
+
+
 void loop() {
-  digitalWrite(1, HIGH);   // LED ON
-  delay(100);             // Wait 1 second
-  digitalWrite(1, LOW);    // LED OFF
-  delay(100000000000);             // Wait 1 second
+  digitalWrite(3, HIGH);   // LED ON
+  delay(1000);             // Wait 1 second
+  digitalWrite(3, LOW);    // LED OFF
+  delay(1000);             // Wait 1 second
 }
